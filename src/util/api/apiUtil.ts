a245480f561e08060bc348e127087f344e25ac92
Raf Vanpuyvelde <raf.vanpuyvelde@sympl.be>
2021-07-02 08:59:23 +0200

    🚑 Fixes bad storage type refactor

--- src/util/api/apiUtil.ts
+++ src/util/api/apiUtil.ts
@@ -2,19 +2,19 @@ export const fetcher = async (...args: unknown[]): Promise<unknown> => {
   // @ts-ignore
   const res = await fetch(...args, {
     headers: {
-      Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
       'User-Agent': 'SavedFilter/0.1 by u/SaltySpartan88',
       'Content-Type': 'application/x-www-form-urlencoded',
     },
   });
 
-  if (res.status === 401) localStorage.removeItem('token');
+  // if (res.status === 401) localStorage.removeItem('token');
 
   return res.json();
 };
 
 export const getUserName = (): string | undefined => {
-  return sessionStorage.getItem('username')?.split('/').reverse()[0];
+  return localStorage.getItem('username')?.split('/').reverse()[0];
 };
 
 export default fetcher;
